c4929db9878dc94d1d9590ea29fcf03a15d9fc06
vsuchkov <vsuchkov@axbit.ru>
2025-08-13 14:49:42 +0400

    add object in app

--- src/automation_exercise/app.py
+++ src/automation_exercise/app.py
@@ -1,12 +1,14 @@
 from src.automation_exercise.page_objects.contact_us_page_object import ContactUsPage
 from src.automation_exercise.page_objects.navigation_bar_object import NavigationBar
+from src.automation_exercise.page_objects.products_page_object import ProductsPage
 from src.automation_exercise.page_objects.signup_login_page_object import SignUpLoginPage
 from src.automation_exercise.page_objects.user_account_page import UserAccountPage
 
 
 class Application:
-    def __init__(self):
-        self.navigation_bar = NavigationBar()
-        self.signup_login_page = SignUpLoginPage()
-        self.user_account_page = UserAccountPage()
-        self.contact_us_page = ContactUsPage()
+	def __init__(self):
+		self.navigation_bar = NavigationBar()
+		self.signup_login_page = SignUpLoginPage()
+		self.user_account_page = UserAccountPage()
+		self.contact_us_page = ContactUsPage()
+		self.products = ProductsPage()
