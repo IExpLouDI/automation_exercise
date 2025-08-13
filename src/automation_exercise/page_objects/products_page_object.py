d6c4ee7d4e5a9c1d8231b28149abaf9a24c48459
vsuchkov <vsuchkov@axbit.ru>
2025-08-13 10:55:04 +0400

    clean file

--- src/automation_exercise/page_objects/products_page_object.py
+++ src/automation_exercise/page_objects/products_page_object.py
@@ -15,12 +15,14 @@ class ProductsPage(StableObject):
 		with step('Проверяем, что текст - ALL PRODUCTS'):
 			self.page_name.should(have.exact_text('ALL PRODUCTS'))
 
-    def scroll_products_page(self, scroll_step:int):
-        browser.execute_script(f"window.scrollBy(0, {scroll_step});")
 
-        return self
+	def scroll_products_page(self, scroll_step: int):
+		browser.execute_script(f"window.scrollBy(0, {scroll_step});")
 
-    def find_wanted_product(self, product_id):
-        with step(f'Прокручиваем каталог продуктов, пока не будет виден товар - {product_id}'):
-            while not browser.element(f"[data-product-id='{product_id}']").matching(be.visible):
-                self.scroll_products_page(100)
+		return self
+
+
+	def find_wanted_product(self, product_id):
+		with step(f'Прокручиваем каталог продуктов, пока не будет виден товар - {product_id}'):
+			while not browser.element(f"[data-product-id='{product_id}']").matching(be.visible):
+				self.scroll_products_page(100)
