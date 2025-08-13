1e8022defabf2595be092091e7527299a3d691bd
vsuchkov <vsuchkov@axbit.ru>
2025-08-13 10:27:18 +0400

    update move scroll page to StableOpject

--- src/automation_exercise/page_objects/products_page_object.py
+++ src/automation_exercise/page_objects/products_page_object.py
@@ -1,21 +1,19 @@
 from selene import browser, be, have
 from allure import step
 
+from src.automation_exercise.page_objects.stable_pages_object import StableObject
 
-class ProductsPage:
-    def __init__(self):
-        self.page_name = browser.element('h2.title')
-        self.scroll_up = browser.element('#scrollUp')
 
-    def verify_open_products_page(self):
-        with step('Проверяем, что открылась вкладка с товарами'):
-            self.page_name.should(be.present)
-        with step('Проверяем, что текст - ALL PRODUCTS'):
-            self.page_name.should(have.exact_text('ALL PRODUCTS'))
+class ProductsPage(StableObject):
+	def __init__(self):
+		super().__init__()
+		self.page_name = browser.element('h2.title')
 
-    def click_scroll_up_button(self):
-        with step('Нажимаем кнопку scroll_up'):
-            self.scroll_up.click()
+	def verify_open_products_page(self):
+		with step('Проверяем, что открылась вкладка с товарами'):
+			self.page_name.should(be.present)
+		with step('Проверяем, что текст - ALL PRODUCTS'):
+			self.page_name.should(have.exact_text('ALL PRODUCTS'))
 
     def scroll_products_page(self, scroll_step:int):
         browser.execute_script(f"window.scrollBy(0, {scroll_step});")
