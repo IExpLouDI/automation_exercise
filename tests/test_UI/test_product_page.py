05e6ff37eec2bac1b86456a17d057d79feb91689
vsuchkov <vsuchkov@axbit.ru>
2025-08-13 14:50:01 +0400

    add skelet in test

--- tests/test_UI/test_product_page.py
+++ tests/test_UI/test_product_page.py
@@ -1,2 +1,13 @@
-def test_search_product(setup_browser):
+def test_verify_product_page_and_product_detail_page(setup_browser, application):
+	application.navigation_bar.products()
+	application.products.verify_open_products_page()
+	application.products.scroll_page(100)
+	application.products.open_product_deatail()
+	application.products.verify_product_detail_is_open()
+	application.products.check_product_detail_is_visible()
+
+
+def test_search_product(setup_browser, application):
+	application.navigation_bar.open_products_page()
+	application.products.verify_open_products_page()
 	pass
