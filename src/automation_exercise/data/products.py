bb200ba0dc5acfaf3d9824be3b5d2ff031cf98e0
Viacheslav Suchkov <iexploudi@gmail.com>
2025-08-18 22:17:28 +0400

    add some products

---
+++ src/automation_exercise/data/products.py
@@ -0,0 +1,15 @@
+from src.automation_exercise.data.product_card import ProductCard
+
+product_men_tshirt = ProductCard(name='Men Tshirt',
+                                 category='Men > Tshirts',
+                                 price='Rs. 400',
+                                 quantity='1',
+                                 product_id='2'
+                                 )
+
+product_women_blue_top = ProductCard(name='Blue Top',
+                                 category='Women > Tops',
+                                 price='Rs. 500',
+                                 quantity='1',
+                                 product_id='1'
+                                 )
