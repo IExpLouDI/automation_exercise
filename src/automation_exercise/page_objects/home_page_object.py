acdcd8bf11c378dc3753853b270db8fe0f587b9c
Viacheslav Suchkov <iexploudi@gmail.com>
2025-08-19 22:01:53 +0400

    add function

--- src/automation_exercise/page_objects/home_page_object.py
+++ src/automation_exercise/page_objects/home_page_object.py
@@ -1,6 +1,15 @@
+from allure import step
+from selene import browser
+
 from src.automation_exercise.page_objects.stable_pages_object import StableObject
 
 
 class HomePage(StableObject):
     def __init__(self):
         super().__init__()
+
+
+    def click_view_product(self, product_id):
+        with step(f'Открывает страницу продукта - {product_id}'):
+            browser.element(f"[href='/product_details/{product_id}']").click()
+
