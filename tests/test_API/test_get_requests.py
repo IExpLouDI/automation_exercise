6c3708770d71b7921a664a13c3ebc67256dd0811
vsuchkov <vsuchkov@axbit.ru>
2025-08-25 10:21:09 +0400

    Добавлен тест на статус сообщения

--- tests/test_API/test_get_requests.py
+++ tests/test_API/test_get_requests.py
@@ -7,3 +7,9 @@ def test_get_all_product_valid_shema(api_application, load_schema):
     with open(load_schema['get_all_product_list'], 'r', encoding='utf-8') as file:
         schema = json.loads(file.read())
     validate(instance=response, schema=schema)
+
+
+def test_get_all_product_valid_status_code(api_application, load_schema):
+    response = api_application.get.get_all_product()
+    assert response['status_code'] == 200
+
