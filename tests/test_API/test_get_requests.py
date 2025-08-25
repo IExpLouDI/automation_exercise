b038674b3b1b40a6a37fe5f273cd6e7372544241
Viacheslav Suchkov <iexploudi@gmail.com>
2025-08-25 22:52:17 +0400

    clean file

--- tests/test_API/test_get_requests.py
+++ tests/test_API/test_get_requests.py
@@ -21,7 +21,6 @@ def test_get_all_brand_list_valid_shema(api_application, load_schema):
 
 def test_get_all_brand_list_valid_status_code(api_application):
     response_info = api_application.get.get_all_brand_list()
-
     check_response_status_and_message_business_code(response_info, 200, 200)
 
 
@@ -33,20 +32,11 @@ def test_get_user_account_detail_valid_schema(api_application, load_schema, crea
 def test_get_user_account_detail_valid_status_code(api_application, create_user, create_account):
     response_info = api_application.get.get_user_account_detail_by_email(create_user.email)
     check_response_status_and_message_business_code(response_info, 200, 200)
-    # with step(f'Проверка статуса ответа = 200'):
-    #     assert response_info.get('response') == 200
-    # with step(f'Проверка бизнес кода ответа = 200'):
-    #     assert response_info.get('response').get('responseCode') == 200
 
 
 def test_get_user_account_detail_with_not_exist_email(api_application, create_user):
     response_info = api_application.get.get_user_account_detail_by_email(create_user.email)
-
     check_response_status_and_message_business_code(response_info, 200, 404)
-    # with step(f'Проверка статуса ответа = 200'):
-    #     assert response_info.get('status_code') == 200
-    # with step(f'Проверка бизнес кода ошибки = 404'):
-    #     assert response_info.get('response').get('responseCode') == 404
 
     with step('Проверка текста бизнес ошибки = Account not found with this email, try another email!'):
         assert response_info.get('response').get('message') == 'Account not found with this email, try another email!'
@@ -54,7 +44,6 @@ def test_get_user_account_detail_with_not_exist_email(api_application, create_us
 
 def test_get_user_account_detail_with_check_content(api_application, create_user, create_account):
     response_info = api_application.get.get_user_account_detail_by_email(create_user.email)
-
     check_response_status_and_message_business_code(response_info, 200, 200)
 
     with step(f'Проверка контента ответа на совпадение с данными пользователя {create_user}'):
