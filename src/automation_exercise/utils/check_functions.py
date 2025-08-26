1a3936fc61fbdf84a1da8a8ec010b41c01bf397f
Viacheslav Suchkov <iexploudi@gmail.com>
2025-08-26 22:44:01 +0400

    add check_response_message_content

--- src/automation_exercise/utils/check_functions.py
+++ src/automation_exercise/utils/check_functions.py
@@ -3,6 +3,8 @@ from allure import step
 from jsonschema import validate
 from selene import browser, be
 
+from automation_exercise.utils.helpers import camel_to_snake_case
+
 
 @step('Проверяем, что открыт сайт https://www.automationexercise.com')
 def check_website_is_open():
@@ -24,3 +26,9 @@ def check_response_content(response:dict, valid_content:dict) -> None:
     for key in valid_content.keys():
         with step(f'Проверяем, что {key} = {valid_content.get(key)}'):
             assert response.get('user').get(key) == valid_content.get(key)
+
+def check_response_message_content(response:dict, wait_params:dict):
+    for key in [el for el in wait_params.keys() if el not in ['email', 'password']]:
+        with step(f'Проверяем, что {key} = {wait_params.get(key)}'):
+            snake_case_key = camel_to_snake_case(key)
+            assert response.get('response').get('user').get(snake_case_key) == wait_params.get(key)
