f6c73bc3bec978be0f2004d00eaa9da7e4cba130
Viacheslav Suchkov <iexploudi@gmail.com>
2025-08-26 23:23:00 +0400

    refactoring function

--- src/automation_exercise/utils/helpers.py
+++ src/automation_exercise/utils/helpers.py
@@ -1,5 +1,7 @@
-import re
-
-
-def camel_to_snake_case(key_name):
-    return re.sub(r'(?<!^)(?=[A-Z])', '_', key_name).lower()
+def switch_req_key_to_resp_key(key_name):
+    """Мапинг ключа из запроса, на ключ из ответа"""
+    switch_key = {
+        'firstname': 'first_name',
+        'lastname': 'last_name'
+    }
+    return switch_key.get(key_name, key_name)
